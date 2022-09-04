2c6206f906a64f07c1b4aa1e25039be2f9198b7a
Putra Nugraha <putra.graha.7@gmail.com>
2022-09-04 23:37:03 +0700

    add search books in book repository

--- lib/features/book/domain/repositories/book_repository.dart
+++ lib/features/book/domain/repositories/book_repository.dart
@@ -5,4 +5,6 @@ import 'package:gutenberg_library/features/book/domain/entities/book.dart';
 abstract class BookRepository {
 
   Future<Either<Failure, List<Book>>> getBooks(int pageNum);
+
+  Future<Either<Failure, List<Book>>> searchBooks(String keyword);
 }
