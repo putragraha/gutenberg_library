a390554029c2e1598eb8589fd537f2b7506d0d91
Putra Nugraha <putra.graha.7@gmail.com>
2022-09-07 23:16:01 +0700

    update bookshelf remote data source class name

--- lib/features/book/data/datasources/bookshelf_remote_data_source.dart
+++ lib/features/book/data/datasources/bookshelf_remote_data_source.dart
@@ -1,6 +1,6 @@
 import 'package:gutenberg_library/features/book/data/models/books_model.dart';
 
-abstract class BookRemoteDataSource {
+abstract class BookshelfRemoteDataSource {
 
   /// For calling API http://gutendex.com/books?topic={bookshelf}
   /// Throws [ServerException] on issue occurred
